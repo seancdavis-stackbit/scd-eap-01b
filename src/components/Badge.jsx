b04fd1a39b1d86360a25017b4fc155b49dfb77e3
Sean C Davis <scdavis41@gmail.com>
2021-09-21 14:36:27 -0400

    Extend badge component

---
+++ src/components/Badge.jsx
@@ -0,0 +1,24 @@
+import classNames from "classnames";
+
+export default function Badge(props) {
+  const { label } = props;
+  const cssClasses = props.className || null;
+  const cssId = props.elementId || null;
+
+  if (!label) {
+    return null;
+  }
+
+  return (
+    <div
+      id={cssId}
+      className={classNames(
+        "sb-component",
+        "sb-component-block",
+        "sb-component-badge",
+        cssClasses
+      )}>
+      HI! — {label}
+    </div>
+  );
+}
